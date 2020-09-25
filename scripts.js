288fc3f99fb5046bad5ae343c8f1c2c72d56c6f8
Jonas Yzermans <jonazyzermans@gmail.com>
2020-09-26 01:19:30 +0200

    Design wip

--- scripts.js
+++ scripts.js
@@ -13,6 +13,7 @@ function addText(_text, _element)
     let text = document.createElement("p");
     text.innerHTML = _text;
     document.getElementById(_element).appendChild(text);
+
 }
 
 function addImage(_img, _element)
@@ -22,39 +23,94 @@ function addImage(_img, _element)
     document.getElementById(_element).appendChild(img);
 }
 
+function createDiv(_div, _target)
+{
+    let container = document.createElement("div");
+    container.id = _div;
+    document.getElementById(_target).appendChild(container);
+}
+
 function createDescriptionPage()
 {
-    addText(loremIpsum, "content");
-    addImage("jonas", "content");
+    /*let container = document.createElement("div");
+    container.id = "containerDiv";
+    document.getElementById("content").appendChild(container);*/
+    /*
+    let newDiv = document.createElement("div");
+    newDiv.id = "descriptionDiv";
+    container.appendChild(newDiv);*/
+  /*   newDiv = document.createElement("div");
+    newDiv.id = "sidebarDiv";
+    container.appendChild(newDiv);*/   
+    
+    createDiv("containerDiv", "content");
+    createDiv("descriptionDiv", "containerDiv");
+    addText(loremIpsum, "descriptionDiv");
+    createDiv("sidebarDiv", "containerDiv");
+    let description = document.getElementById("descriptionDiv");
+    let sidebar = document.getElementById("sidebarDiv");
+
+    addImage("jonas", "sidebarDiv");
+
+    addText("Github: ", "sidebarDiv");
+    let linkText = document.createTextNode("https://github.com/firefool/");
+    let link = document.createElement("a");
+    link.appendChild(linkText);
+    link.href = "https://github.com/firefool/";
+    sidebar.appendChild(link);
+
+    sidebar.appendChild(document.createElement("br"));
+    sidebar.appendChild(document.createElement("br"));
+
+    addText("Email Adress: ", "sidebarDiv");
+    linkText = document.createTextNode("Jonazyzermans@gmail.com");
+    link = document.createElement("a");
+    link.appendChild(linkText);
+    link.href = "mailto:Jonazyzermans@gmail.com";
+    sidebar.appendChild(link);
+
 }
 
 function createWorkPage()
 {
-    createWorkExampleText("Skoluppgift #1", loremIpsum, "content");
-    createWorkExampleText("Skoluppgift #2", loremIpsum, "content");
-    createWorkExampleText("Skoluppgift #3", loremIpsum, "content");
-    createWorkExampleText("Skoluppgift #4", loremIpsum, "content");
+    let container = document.createElement("div");
+    container.id = "containerWorkDiv";
+    document.getElementById("content").appendChild(container);
+
+    createWorkExampleText("Skoluppgift #1", loremIpsum, "www.google.com", "containerWorkDiv");
+    createWorkExampleText("Skoluppgift #2", loremIpsum, "www.google.com", "containerWorkDiv");
+    createWorkExampleText("Skoluppgift #3", loremIpsum, "www.google.com", "containerWorkDiv");
+    createWorkExampleText("Skoluppgift #4", loremIpsum, "www.google.com", "containerWorkDiv");
+    createWorkExampleText("Skoluppgift #1", loremIpsum, "www.google.com", "containerWorkDiv");
+    createWorkExampleText("Skoluppgift #2", loremIpsum, "www.google.com", "containerWorkDiv");
+    createWorkExampleText("Skoluppgift #3", loremIpsum, "www.google.com", "containerWorkDiv");
+    createWorkExampleText("Skoluppgift #4", loremIpsum, "www.google.com", "containerWorkDiv");
 }
 
-function createWorkExampleText(_title, _text, _element)
+function createWorkExampleText(_title, _text, _link, _element)
 {
+    let newDiv = document.createElement("div");
+    newDiv.className = "workClass";
+/*
     let line = document.createElement("hr");
     line.className = "line";
-    document.getElementById(_element).appendChild(line);
+    newDiv.appendChild(line);*/
 
     let title = document.createElement("h3");
     title.innerHTML = _title;
-    document.getElementById(_element).appendChild(title);
-
+    newDiv.appendChild(title);
+    
     let link = document.createElement("a");
-    let linkText = document.createTextNode("Here is a link");
+    let linkText = document.createTextNode(_link);
     link.appendChild(linkText);
-    link.href = "http://google.com";
-    document.getElementById(_element).appendChild(link);
+    link.href = "https://" + _link;
+    newDiv.appendChild(link);
 
     let text = document.createElement("p");
     text.innerHTML = _text;
-    document.getElementById(_element).appendChild(text);
+    newDiv.appendChild(text);
+
+    document.getElementById(_element).appendChild(newDiv);
 }
 
 let homeButton = document.getElementById("homeButton");
